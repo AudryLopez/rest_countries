49a609cbb4fe646124a9b76069a48bcf0fb5f0b6
AudryLopez <alopez@intellisys.com.do>
2022-01-25 21:25:48 -0400

    minor fix in the js logic

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,8 @@
 function change(){   
-    if (document.body.style.backgroundColor == "white") {
-        document.querySelector(".title").style.color = "white";
-        return document.body.style.backgroundColor = "black"
+    if (document.body.style.backgroundColor == "black") {
+        document.querySelector(".title").style.color = "black";
+        return document.body.style.backgroundColor = "white"
     }
-    document.querySelector(".title").style.color = "black";
-    document.body.style.backgroundColor = "white";
+    document.querySelector(".title").style.color = "white";
+    document.body.style.backgroundColor = "black";
 }
