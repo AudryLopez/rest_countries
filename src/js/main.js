06e942fb90fd3656955a2374bdc1cf0b7f2c1bab
AudryLopez <alopez@intellisys.com.do>
2022-01-25 22:24:54 -0400

    added mon icon

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 function change(){   
     if (document.body.style.backgroundColor == "rgb(32, 44, 55)") {
       document.querySelector(".title").style.color = "hsl(200, 15%, 8%)";
-      document.querySelector(".change").style.backgroundColor = "hsl(209, 23%, 22%)";
       return (document.body.style.backgroundColor = " hsl(0, 0%, 98%)");
     }
     document.querySelector(".title").style.color = "hsl(0, 0%, 100%)";
