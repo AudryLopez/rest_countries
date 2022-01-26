46bf93de8141869d6a0cc86a7d10d3fe36b5e356
AudryLopez <alopez@intellisys.com.do>
2022-01-25 21:57:19 -0400

    changing color buttons

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
 function change(){   
-    if (document.body.style.backgroundColor == "black") {
-        document.querySelector(".title").style.color = "black";
-        return document.body.style.backgroundColor = "white"
+    if (document.body.style.backgroundColor == "rgb(32, 44, 55)") {
+      document.querySelector(".title").style.color = "hsl(200, 15%, 8%)";
+      document.querySelector(".change").style.backgroundColor = "hsl(209, 23%, 22%)";
+      return (document.body.style.backgroundColor = " hsl(0, 0%, 98%)");
     }
-    document.querySelector(".title").style.color = "white";
-    document.body.style.backgroundColor = "black";
+    document.querySelector(".title").style.color = "hsl(0, 0%, 100%)";
+    document.body.style.backgroundColor = "hsl(207, 26%, 17%)";
 }
