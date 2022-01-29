248281f2071ba6ea42cfce167e908b7244df2dee
AudryLopez <alopez@intellisys.com.do>
2022-01-28 21:12:03 -0400

    fixing colors

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,12 @@
 function change(){ 
   const nav = document.querySelector(".navbar");
-  //const title = document.getElementsByClassName(".title"); 
+  const input = document.querySelector(".text-input"); 
   if (document.body.style.backgroundColor == "rgb(32, 44, 55)") {
     nav.style.backgroundColor = "hsl(0, 0%, 100%)";
-    
-    //change(title, "black");
+    input.style.backgroundColor = "hsl(0, 0%, 100%)";
     return (document.body.style.backgroundColor = "hsl(0, 0%, 90%)");
   }
-   nav.style.backgroundColor = "red";
-  //change(title, "hsl(0, 0%, 100%)");
+  nav.style.backgroundColor = "hsl(209, 23%, 22%)";
+  input.style.backgroundColor = "hsl(209, 23%, 22%)";
   document.body.style.backgroundColor = "hsl(207, 26%, 17%)";
 }
