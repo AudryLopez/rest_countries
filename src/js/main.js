7bda2b3dc717fa7b7ab4443567bc58177c904a73
AudryLopez <alopez@intellisys.com.do>
2022-01-28 20:56:49 -0400

    Changes in the navbar

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,13 @@
-function change(){   
-    if (document.body.style.backgroundColor == "rgb(32, 44, 55)") {
-      document.querySelector(".title").style.color = "hsl(200, 15%, 8%)";
-      return (document.body.style.backgroundColor = " hsl(0, 0%, 98%)");
-    }
-    document.querySelector(".title").style.color = "hsl(0, 0%, 100%)";
-    document.body.style.backgroundColor = "hsl(207, 26%, 17%)";
+function change(){ 
+  const nav = document.querySelector(".navbar");
+  //const title = document.getElementsByClassName(".title"); 
+  if (document.body.style.backgroundColor == "rgb(32, 44, 55)") {
+    nav.style.backgroundColor = "hsl(0, 0%, 100%)";
+    
+    //change(title, "black");
+    return (document.body.style.backgroundColor = "hsl(0, 0%, 90%)");
+  }
+   nav.style.backgroundColor = "red";
+  //change(title, "hsl(0, 0%, 100%)");
+  document.body.style.backgroundColor = "hsl(207, 26%, 17%)";
 }
