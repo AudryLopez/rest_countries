4555067f9dd07706b2f21b90f775fe63c1669590
AudryLopez <alopez@intellisys.com.do>
2022-02-01 20:23:29 -0400

    added card to container

---
+++ components/card/card.tsx
@@ -0,0 +1,18 @@
+import Image from "next/image"
+import styles from "./card.module.css"
+
+export default function Card(){
+
+  
+  return (
+    <div className={styles.container}>
+      <Image src="img_avatar.png" alt="Avatar" />
+      <div className={styles.container}>
+        <h4>
+          <b>John Doe</b>
+        </h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  );
+}
