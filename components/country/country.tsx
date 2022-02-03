3d5b024c99cf0a40db7f538fb76a9e517bcc39ac
AudryLopez <alopez@intellisys.com.do>
2022-02-02 21:42:29 -0400

    change idetation ti taps

--- components/country/country.tsx
+++ components/country/country.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image"
 import styles from "./country.module.css"
-import Country from "../types/country";
+import Country from "../types/country"
 
 export default function Countries(props: Country) {
-  return (
-    <section className={styles.main} >
-      <Image src={props.flags} alt={`flag of ${props.nativeName}`} />
-      <div className={styles.country} >
-        <h1 className={styles.title} >{props.nativeName}</h1>
-      </div>
-    </section>
-  );
+	return (
+		<section className={styles.main} >
+			<Image src={props.flags} alt={`flag of ${props.nativeName}`} />
+			<div className={styles.country} >
+				<h1 className={styles.title} >{props.nativeName}</h1>
+			</div>
+		</section>
+	);
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -2,18 +2,18 @@ import styles from './header.module.css'
 import Image from 'next/image'
 
 export default function Header(){
-    return (
-      <header className={styles.navbar}>
-        <div className={styles.left}>
-          <h1 className={styles.title}>Where in the word</h1>
-        </div>
-        <div className={styles.right}>
-          <Image
-            src="../img/moon-solid.svg"
-            className={styles.btnchange}
-            alt="color theme button"
-          />
-        </div>
-      </header>
-    );
+		return (
+			<header className={styles.navbar}>
+				<div className={styles.left}>
+					<h1 className={styles.title}>Where in the word</h1>
+				</div>
+				<div className={styles.right}>
+					<Image
+						src="../img/moon-solid.svg"
+						className={styles.btnchange}
+						alt="color theme button"
+					/>
+				</div>
+			</header>
+		);
 } 
