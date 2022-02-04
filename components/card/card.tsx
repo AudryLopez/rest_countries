01255bb519e588f4564cce404a68bea839bf3b49
AudryLopez <alopez@intellisys.com.do>
2022-02-03 21:27:14 -0400

    idetation to space

--- components/card/card.tsx
+++ components/card/card.tsx
@@ -3,15 +3,15 @@ import styles from './card.module.css'
 
 export default function Card(name: string){
 
-	return (
-		<div className={styles.container}>
-			<Image src="img_avatar.png" alt="Avatar" />
-			<div className={styles.container}>
-				<h4>
-					<b>John Doe</b>
-				</h4>
-				<p>Architect & Engineer</p>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <Image src="img_avatar.png" alt="Avatar" />
+      <div className={styles.container}>
+        <h4>
+          <b>John Doe</b>
+        </h4>
+        <p>Architect & Engineer</p>
+      </div>
+    </div>
+  );
 }
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,20 +3,21 @@ import Image from 'next/image'
 import moon from "/components/img/moon-solid.svg";
 
 export default function Header(){
-		return (
-      <header className={styles.navbar}>
-        <div className={styles.left}>
-          <h1 className={styles.title}>Where in the word</h1>
-        </div>
-        <div className={styles.right}>
-          <Image
-            src={moon}
-            className={styles.btnchange}
-            alt="color theme button"
-            width={60}
-            height={35}
-          />
-        </div>
-      </header>
-    );
+  
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.left}>
+        <h1 className={styles.title}>Where in the word</h1>
+      </div>
+      <div className={styles.right}>
+        <Image
+          src={moon}
+          className={styles.btnchange}
+          alt="color theme button"
+          width={60}
+          height={35}
+        />
+      </div>
+    </header>
+  );
 } 
