d379b926e9618ace91ba0013776f3059f234362c
AudryLopez <alopez@intellisys.com.do>
2022-02-03 20:15:47 -0400

    css header

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,19 +1,22 @@
 import styles from './header.module.css'
 import Image from 'next/image'
+import moon from "/components/img/moon-solid.svg";
 
 export default function Header(){
 		return (
-			<header className={styles.navbar}>
-				<div className={styles.left}>
-					<h1 className={styles.title}>Where in the word</h1>
-				</div>
-				<div className={styles.right}>
-					<Image
-						src="../img/moon-solid.svg"
-						className={styles.btnchange}
-						alt="color theme button"
-					/>
-				</div>
-			</header>
-		);
+      <header className={styles.navbar}>
+        <div className={styles.left}>
+          <h1 className={styles.title}>Where in the word</h1>
+        </div>
+        <div className={styles.right}>
+          <Image
+            src={moon}
+            className={styles.btnchange}
+            alt="color theme button"
+            width={60}
+            height={35}
+          />
+        </div>
+      </header>
+    );
 } 
